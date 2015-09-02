b31fc357793b7c19a65d24094dd46a0412849503
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-02 22:39:23 +0900

    close #5 add userconf.json file to store user input, orsay / tizen

--- tasks/packager/tizen.js
+++ tasks/packager/tizen.js
@@ -10,108 +10,159 @@ var zipdir = require('zip-dir');
 module.exports = {
     build: function (successCallback, errorCallback, wwwSrc, dest, platformRepos, scripts) {
         console.log("\nStart building Samsung Tizen Platform......");
+
+        // file path
         wwwSrc = path.resolve(wwwSrc);
         dest = path.resolve(dest);
         platformRepos = path.resolve(platformRepos);
 
+        // script
         var cordovaSrc = path.resolve(scripts['cordova.js']);
         var toastSrc = path.resolve(scripts['toast.js']);
 
+        // config
         var cordovaConf = utils.getCordovaConfig();
-        var choice = [{
-            type: 'input',
-            name: 'name',
-            message: 'What\'s the application\'s name?',
-            default: cordovaConf.name
-        }, {
-            type: 'input',
-            name: 'id',
-            message: 'Application Id (Valid RegExp: [0-9a-zA-Z]{10})',
-            default: 'puttizenid',
-            validate: function(input) {
-                return /[0-9a-zA-Z]{10}/.test(input) ? true : "invalid id string for tizen platform";
+
+        var userconf = {
+            data : {},
+            filepath : path.join(dest, '../userconf.json'),
+            setUserConf : function(_data){
+                this.data = _data;
+            },
+            getUserConf : function(){
+                return this.data;                
+            },
+            inputNewData : function(){
+                var choice = [{
+                    type: 'input',
+                    name: 'name',
+                    message: 'What\'s the application\'s name?',
+                    default: cordovaConf.name
+                }, {
+                    type: 'input',
+                    name: 'id',
+                    message: 'Application Id (Valid RegExp: [0-9a-zA-Z]{10})',
+                    default: 'puttizenid',
+                    validate: function(input) {
+                        return /[0-9a-zA-Z]{10}/.test(input) ? true : "invalid id string for tizen platform";
+                    }
+                }, {
+                    type: 'input',
+                    name: 'version',
+                    message: 'Application Version(Valid RegExp: \d.\d.\d)',
+                    default: cordovaConf.version,
+                    validate: function(input) {
+                        return /\d.\d.\d/.test(input) ? true : "invalid version string for tizen platform";
+                    }
+                }, {
+                    type: 'input',
+                    name: 'description',
+                    message: 'Application Description',
+                    default: 'Web application for Samsung Tizen TV'
+                }];
+
+                inquirer.prompt(choice, function (answers) {
+                    config = answers;
+
+                    userconf.setUserConf(config);
+
+                    !fs.existsSync(dest) && fs.mkdirSync(dest);
+                    fs.writeFileSync(userconf.filepath, JSON.stringify(config), {encoding: 'utf8'});
+
+                    tizenUtil.buildProject();
+                });
             }
-        }, {
-            type: 'input',
-            name: 'version',
-            message: 'Application Version(Valid RegExp: \d.\d.\d)',
-            default: cordovaConf.version,
-            validate: function(input) {
-                return /\d.\d.\d/.test(input) ? true : "invalid version string for tizen platform";
+        };
+
+        var tizenUtil = {
+            copySrcToDest : function() {
+                var tmp = dest.split(path.sep);
+                
+                var curPath = tmp[0];
+                for(var i=1; i<tmp.length; i++) {
+                    curPath = path.join(curPath, tmp[i]);
+                    !fs.existsSync(curPath) && fs.mkdirSync(curPath);
+                }
+
+                // // copy cordova.js
+                // shelljs.cp('-rf', cordovaSrc, dest);
+
+                // copy toast.js
+                fs.exists(toastSrc, function(exists){
+                    if(exists) shelljs.cp('-rf', toastSrc, dest);
+                    else console.log('\n\ncan\'t find toast.js at\n'+toastSrc);
+                }); 
+
+                shelljs.cp('-rf', path.join(wwwSrc, '*'), dest);
+                return true;
+            },
+            buildPlatformAdditions : function() {
+                shelljs.cp('-rf', path.join(platformRepos, 'www', '*'), dest);
+
+                // replace config.xml template with actual configuration
+                this.replaceTemplate('config.xml');
+
+                // replace .project template with actual configuration
+                // .project is hidden file in linux
+                this.replaceTemplate('project', true);
+                this.replaceTemplate('tproject', true);
+
+                return true;
+            }, 
+            replaceTemplate : function(filename, isHidden) {
+                // replace config.xml template with actual configuration
+                var data = userconf.getUserConf();
+
+                var tmplFile = fs.readFileSync(path.join(dest, filename), {encoding: 'utf8'});
+                var rendered = mustache.render(tmplFile, data);
+
+                fs.writeFileSync(path.join(dest, filename + '.tmp'), rendered, {encoding: 'utf8'});
+                
+                //hidden file.......
+                if(isHidden){
+                    shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, '.'+filename));
+                    shelljs.rm('-f', path.join(dest, filename));
+                }else{
+                    shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, filename));
+                }
+            },
+            buildProject : function(){
+                this.copySrcToDest() || (errorCallback && errorCallback());
+                this.buildPlatformAdditions() || (errorCallback && errorCallback());
+
+                console.log('Built at ' + dest);
+                successCallback && successCallback();
             }
-        }, {
-            type: 'input',
-            name: 'description',
-            message: 'Application Description',
-            default: 'Web application for Samsung Tizen TV'
-        }];
+        };
 
-        var config = {};
-        inquirer.prompt(choice, function (answers) {
-            config = answers;
+        if(fs.existsSync(userconf.filepath)){
+            // userconf.json already exists
+            var cacheAsk = [{
+                type: 'confirm',
+                name: 'cache',
+                message: 'Already have [userconf.json], Do you want to use this data?'
+            }];
 
-            copySrcToDest() || (errorCallback && errorCallback());
-            buildPlatformAdditions() || (errorCallback && errorCallback());
-
-            console.log('\n\nBuilt at ' + dest);
-            successCallback && successCallback();
-        });
-
-        function copySrcToDest() {
-            var tmp = dest.split(path.sep);
-            //console.log(tmp);
-            var curPath = tmp[0];
-            for(var i=1; i<tmp.length; i++) {
-                curPath = path.join(curPath, tmp[i]);
-                //console.log("curPath: " + curPath);
-                !fs.existsSync(curPath) && fs.mkdirSync(curPath);
-            }
-
-            //toast.js
-            fs.exists(toastSrc, function(exists){
-                if(exists) shelljs.cp('-rf', toastSrc, dest);
-                else console.log('\n\ncan\'t find toast.js at\n'+toastSrc);
+            inquirer.prompt(cacheAsk, function(answers){
+                if(answers.cache){
+                    // use cache data
+                    var data = fs.readFileSync(userconf.filepath);
+                    data = JSON.parse(data);
+                    userconf.setUserConf(data);
+                    tizenUtil.buildProject();
+                }else{
+                    // input new data
+                    userconf.inputNewData();        
+                }
             });
-
-            shelljs.cp('-rf', path.join(wwwSrc, '*'), dest);
-            return true;
-        }
-
-        function buildPlatformAdditions() {
-            shelljs.cp('-rf', path.join(platformRepos, 'www', '*'), dest);
-
-            // replace config.xml template with actual configuration
-            replaceTemplate('config.xml');
-
-            // replace .project template with actual configuration
-            // .project is hidden file in linux
-            replaceTemplate('project', true);
-            replaceTemplate('tproject', true);
-
-            return true;
-        }
-
-        function replaceTemplate(filename, isHidden) {
-            // replace config.xml template with actual configuration
-            var tmplFile = fs.readFileSync(path.join(dest, filename), {encoding: 'utf8'});
-            var rendered = mustache.render(tmplFile, config);
-
-            //console.log(rendered);
-            fs.writeFileSync(path.join(dest, filename + '.tmp'), rendered, {encoding: 'utf8'});
-            
-            //hidden file.......
-            if(isHidden){
-                shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, '.'+filename));
-                shelljs.rm('-f', path.join(dest, filename));
-            }else{
-                shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, filename));
-            }
-
-            return true;
+        }else{
+            // create userconf.json, input new data
+            userconf.inputNewData();
         }
     },
-    package: function (successCallback, errorCallback, build, dest) {
+    package: function (successCallback, errorCallback, build, dest){
         console.log("\nStart packaging Samsung Tizen TV Platform......");
+
         build = path.resolve(build);
         dest = path.resolve(dest);
 
