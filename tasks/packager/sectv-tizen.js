681403f71556bfbe59fc2f99b65bd981837d08ae
Hyojin Kim <hyojins.kim@samsung.com>
2015-09-15 13:52:36 +0900

    modify sectv-tizen.js

--- tasks/packager/sectv-tizen.js
+++ tasks/packager/sectv-tizen.js
@@ -6,8 +6,10 @@ var inquirer = require('inquirer');
 var utils = require('../lib/utils');
 var shelljs = require('shelljs');
 var mustache = require('mustache');
-// var grunt = require('grunt');
 var zipdir = require('zip-dir');
+
+var userconfData = {};
+var userconfPath = '';
 
 module.exports = {
     build: function (successCallback, errorCallback, wwwSrc, dest, platformRepos, scripts) {
@@ -17,139 +19,19 @@ module.exports = {
         wwwSrc = path.resolve(wwwSrc);
         dest = path.resolve(dest);
         platformRepos = path.resolve(platformRepos);
+        userconfPath = path.join(dest, '../userconf.json');
 
         // config
         var cordovaConf = utils.getCordovaConfig();
 
-        var userconf = {
-            data : {},
-            filepath : path.join(dest, '../userconf.json'),
-            setUserConf : function(_data){
-                this.data = _data;
-            },
-            getUserConf : function(){
-                return this.data;                
-            },
-            saveFile : function(){
-                fs.writeFileSync(this.filepath, JSON.stringify(this.data), {encoding: 'utf8'});
-            },
-            inputNewData : function(){
-                var choice = [{
-                    type: 'input',
-                    name: 'name',
-                    message: 'What\'s the application\'s name?',
-                    default: cordovaConf.name
-                }, {
-                    type: 'input',
-                    name: 'id',
-                    message: 'Application Id (Valid RegExp: [0-9a-zA-Z]{10})',
-                    default: 'puttizenid',
-                    validate: function(input) {
-                        return /[0-9a-zA-Z]{10}/.test(input) ? true : 'invalid id string for tizen platform';
-                    }
-                }, {
-                    type: 'input',
-                    name: 'version',
-                    message: 'Application Version(Valid RegExp: \\d.\\d.\\d)',
-                    default: cordovaConf.version,
-                    validate: function(input) {
-                        return /\d.\d.\d/.test(input) ? true : 'invalid version string for tizen platform';
-                    }
-                }, {
-                    type: 'input',
-                    name: 'description',
-                    message: 'Application Description',
-                    default: cordovaConf.description
-                }];
-
-                inquirer.prompt(choice, function (answers) {
-                    var config = answers;
-
-                    userconf.setUserConf(config);
-                    tizenUtil.buildProject();
-                });
-            }
-        };
-
-        var tizenUtil = {
-            updateRevision : function(curver){
-                var tmp = curver.split('.');
-                var major = tmp[0];
-                var minor = tmp[1];
-                var revision = parseInt(tmp[2]) + 1;
-
-                return major + '.' + minor + '.' + revision;
-            },
-            copySrcToDest : function() {
-                var tmp = dest.split(path.sep);
-                
-                var curPath = tmp[0];
-                for(var i=1; i<tmp.length; i++) {
-                    curPath = path.join(curPath, tmp[i]);
-                    !fs.existsSync(curPath) && fs.mkdirSync(curPath);
-                }
-
-                for(var key in scripts){
-                    if(scripts.hasOwnProperty(key)){
-                        var to = path.join(dest, key);
-                        var from = path.resolve(scripts[key]);
-
-                        shelljs.cp('-f', from, to);
-                    }
-                }
-
-                shelljs.cp('-rf', path.join(wwwSrc, '*'), dest);
-                return true;
-            },
-            buildPlatformAdditions : function() {
-                shelljs.cp('-rf', path.join(platformRepos, 'www', '*'), dest);
-
-                // replace config.xml template with actual configuration
-                this.replaceTemplate('config.xml');
-
-                // replace .project template with actual configuration
-                // .project is hidden file in linux
-                this.replaceTemplate('project', true);
-                this.replaceTemplate('tproject', true);
-
-                return true;
-            }, 
-            replaceTemplate : function(filename, isHidden) {
-                // replace config.xml template with actual configuration
-                var data = userconf.getUserConf();
-
-                var tmplFile = fs.readFileSync(path.join(dest, filename), {encoding: 'utf8'});
-                var rendered = mustache.render(tmplFile, data);
-
-                fs.writeFileSync(path.join(dest, filename + '.tmp'), rendered, {encoding: 'utf8'});
-                
-                //hidden file.......
-                if(isHidden){
-                    shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, '.'+filename));
-                    shelljs.rm('-f', path.join(dest, filename));
-                }else{
-                    shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, filename));
-                }
-            },
-            buildProject : function(){
-                this.copySrcToDest() || (errorCallback && errorCallback());
-                this.buildPlatformAdditions() || (errorCallback && errorCallback());
-
-                userconf.saveFile();
-
-                console.log('Built at ' + dest);
-                successCallback && successCallback();
-            }
-        };
-
-        if(fs.existsSync(userconf.filepath)){
+        if(fs.existsSync(userconfPath)){
             // userconf.json already exists
 
-            var data = fs.readFileSync(userconf.filepath);
+            var data = fs.readFileSync(userconfPath);
             data = JSON.parse(data);
 
             var curVer = data.version;
-            var updateVer = tizenUtil.updateRevision(curVer);
+            var updateVer = updateRevision(curVer);
 
             var cacheAsk = [{
                 type: 'confirm',
@@ -172,16 +54,118 @@ module.exports = {
                 if(answers.cache){
                     // use cache data
                     data.version = answers.revision;
-                    userconf.setUserConf(data);
-                    tizenUtil.buildProject();
+                    setUserConf(data);
+                    buildProject();
                 }else{
                     // input new data
-                    userconf.inputNewData();        
+                    inputNewData();        
                 }
             });
         }else{
             // create userconf.json, input new data
-            userconf.inputNewData();
+            inputNewData();
+        }
+
+        function inputNewData() {
+            var choice = [{
+                type: 'input',
+                name: 'name',
+                message: 'What\'s the application\'s name?',
+                default: cordovaConf.name
+            }, {
+                type: 'input',
+                name: 'id',
+                message: 'Application Id (Valid RegExp: [0-9a-zA-Z]{10})',
+                default: 'puttizenid',
+                validate: function(input) {
+                    return /[0-9a-zA-Z]{10}/.test(input) ? true : 'invalid id string for tizen platform';
+                }
+            }, {
+                type: 'input',
+                name: 'version',
+                message: 'Application Version(Valid RegExp: /\d./\d./\d)',
+                default: cordovaConf.version,
+                validate: function(input) {
+                    return /\d.\d.\d/.test(input) ? true : 'invalid version string for tizen platform';
+                }
+            }, {
+                type: 'input',
+                name: 'description',
+                message: 'Application Description',
+                default: cordovaConf.description
+            }];
+
+            inquirer.prompt(choice, function (answers) {
+                var config = answers;
+
+                setUserConf(config);
+                buildProject();
+            });
+        }
+
+        function copySrcToDest() {
+            var tmp = dest.split(path.sep);
+                        
+            var curPath = tmp[0];
+            for(var i=1; i<tmp.length; i++) {
+                curPath = path.join(curPath, tmp[i]);
+                !fs.existsSync(curPath) && fs.mkdirSync(curPath);
+            }
+
+            for(var key in scripts){
+                if(scripts.hasOwnProperty(key)){
+                    var to = path.join(dest, key);
+                    var from = path.resolve(scripts[key]);
+
+                    shelljs.cp('-f', from, to);
+                }
+            }
+
+            shelljs.cp('-rf', path.join(wwwSrc, '*'), dest);
+            
+            return true;
+        }
+
+        function buildPlatformAdditions() {
+            shelljs.cp('-rf', path.join(platformRepos, 'www', '*'), dest);
+
+            // replace config.xml template with actual configuration
+            replaceTemplate('config.xml');
+
+            // replace .project template with actual configuration
+            // .project is hidden file in linux
+            replaceTemplate('project', true);
+            replaceTemplate('tproject', true);
+
+            return true;
+        }
+
+        function replaceTemplate(filename, isHidden) {
+            // replace config.xml template with actual configuration
+            var data = getUserConf();
+
+            var tmplFile = fs.readFileSync(path.join(dest, filename), {encoding: 'utf8'});
+            var rendered = mustache.render(tmplFile, data);
+
+            fs.writeFileSync(path.join(dest, filename + '.tmp'), rendered, {encoding: 'utf8'});
+                        
+            //hidden file.......
+            if(isHidden){
+                shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, '.'+filename));
+                shelljs.rm('-f', path.join(dest, filename));
+            }else{
+                shelljs.mv('-f', path.join(dest, filename + '.tmp'), path.join(dest, filename));
+            }
+        }
+
+        function buildProject() {
+            copySrcToDest() || (errorCallback && errorCallback());
+            buildPlatformAdditions() || (errorCallback && errorCallback());
+
+            saveFile();
+
+            console.log('Built at ' + dest);
+            successCallback && successCallback();
         }
     },
     package: function (successCallback, errorCallback, build, dest){
@@ -198,3 +182,26 @@ module.exports = {
         });
     }
 };
+
+//userConf
+function setUserConf(data) {
+    userconfData = data;
+}
+
+function getUserConf() {
+    return userconfData;
+}
+
+function saveFile() {
+    fs.writeFileSync(userconfPath, JSON.stringify(userconfData), {encoding: 'utf8'});
+}
+
+// tizenUtil
+function updateRevision(curver) {
+    var tmp = curver.split('.');
+    var major = tmp[0];
+    var minor = tmp[1];
+    var revision = parseInt(tmp[2]) + 1;
+
+    return major + '.' + minor + '.' + revision;
+}
